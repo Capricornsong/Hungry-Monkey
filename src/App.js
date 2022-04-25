d3322110b35d63f84803a31115b5c67a3c0b5723
Capricornsong <874648832@qq.com>
2022-04-25 22:29:42 +0100

    initial commit
    24/04/2022

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+ * @Author: Liusong He
+ * @Date: 2022-04-25 17:52:35
+ * @LastEditTime: 2022-04-25 22:28:32
+ * @FilePath: \coursework\coursework\src\App.js
+ * @Email: lh2u21@soton.ac.uk
+ * @Description: 
+ */
+import Register from './register_m'
+import Login from './login'
+import{BrowserRouter,Link,Routes,Route} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  return(
+    <BrowserRouter>
+      <Link to='/register_m'>register</Link>
+      <Link to='/login'>login</Link>
+      <Routes>
+        <Route path='register_m' element={<Register/>}></Route>
+        <Route path='login' element={<Login/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
-
 export default App;
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+/*
+ * @Author: Liusong He
+ * @Date: 2022-04-25 22:00:17
+ * @LastEditTime: 2022-04-25 22:27:48
+ * @FilePath: \coursework\coursework\src\index.js
+ * @Email: lh2u21@soton.ac.uk
+ * @Description: 
+ */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +22,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
