5f0c20a28ea0ddb4bfe113498d326a3459e77b42
GiraffeKOOOO <pstepien@hotmail.co.uk>
2022-04-27 13:45:15 +0100

    working on fordot password page

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -11,16 +11,15 @@ import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Typo
                     <CardMedia
                     component="img"
                     height="140"
-                    image="/static/images/cards/contemplative-reptile.jpg"
-                    alt="green iguana"
+                    image="src\media\kebab.jpg"
+                    alt="kebab"
                     />
                     <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
                         {props.name}
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000
-                        species, ranging across all continents except Antarctica
+                        Kebab lorem ipsum Kebab lorem ipsum Kebab lorem ipsum Kebab lorem ipsum Kebab lorem ipsum 
                     </Typography>
                     </CardContent>
                 </CardActionArea>
--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -1,9 +1,39 @@
+import { Box, Button, Container, Grid, TextField, Typography } from '@mui/material';
+import { createTheme } from '@mui/system';
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import Navbar from '../components/Navbar';
 
  function ForgotPassword(){
+    const theme = createTheme();
+
     return(
         <div id="forgot-password-div">
-            <h1>Hello</h1>
+            <Navbar/>
+            <ThemeProvider theme={theme}>
+              <Container component="main" maxWidth="sm">
+              <Box
+                sx={{
+                  marginTop: 8,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                }}
+              >
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                />
+                </Box>
+              </Container>
+            </ThemeProvider>
+            
         </div>
     )
   }
