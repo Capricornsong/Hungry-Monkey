f6d51198c1d7f8d4ff402076d8c3571d091a0df5
GiraffeKOOOO <pstepien@hotmail.co.uk>
2022-05-06 14:39:16 +0100

    new restaurant owner process done

--- src/components/RestaurantOrders.js
+++ src/components/RestaurantOrders.js
@@ -11,6 +11,8 @@ function RestaurantDetails(props) {
     const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
 
     const [editFormOpen, setEditFormOpen] = React.useState(false)
+    const [isLoading, setIsLoading] = React.useState(true)
+    const [newRestaurant, setNewRestaurant] = React.useState(false)
 
     const theme = React.useMemo( () =>
         createTheme({
@@ -21,40 +23,91 @@ function RestaurantDetails(props) {
         [prefersDarkMode],
     );
 
-    return (
-        <ThemeProvider theme={theme}>
-            <Grid container spacing={3} style={{marginTop: 20}}>
-                    <Grid item lg={12} md={12} xs={12}>
+    React.useEffect(() => {
+           if(props.restaurantobjectprop.length > 0){
+                setIsLoading(false)
+           }
+           if(props.restaurantobjectprop.status === 'WaitConfirm'){
+                setNewRestaurant(true)
+                setIsLoading(false)
+           }
+    }, [])
 
-                        <Card sx={{ boxShadow: 3}}>
-                            <CardActionArea onClick={() => setEditFormOpen(!editFormOpen)}>
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="div" textAlign='center'>
-                                        Order List
-                                        <TaskIcon style={{marginLeft: 10}}/>
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary" textAlign='center'>
-                                        {editFormOpen? 'Click here to close the order list' : 'Click here to view and manage orders'}
-                                    </Typography>
-                                </CardContent>
-                            </CardActionArea>
-                        </Card>
-
-                        <Collapse in={editFormOpen} timeout='auto' unmountOnExit>
-                            <Card sx={{ boxShadow: 3 }}>
-                                <CardContent>
-                                    <UnassignedOrders restaurantName={props.restaurantobjectprop.name}/>
-                                    <AssignedOrders restaurantName={props.restaurantobjectprop.name}/>
-                                    <CompletedOrders restaurantName={props.restaurantobjectprop.name}/>
-                                </CardContent>
+    if(isLoading){
+        return <Typography>Loading</Typography>
+    } else if (newRestaurant) {
+        return (
+            <ThemeProvider theme={theme}>
+                <Grid container spacing={3} style={{marginTop: 20}}>
+                        <Grid item lg={12} md={12} xs={12}>
+    
+                            <Card sx={{ boxShadow: 3}}>
+                                <CardActionArea onClick={() => setEditFormOpen(!editFormOpen)}>
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="div" textAlign='center'>
+                                            Order List
+                                            <TaskIcon style={{marginLeft: 10}}/>
+                                        </Typography>
+                                        <Typography variant="body2" color="text.secondary" textAlign='center'>
+                                            {editFormOpen? 'Click here to close the order list' : 'Click here to view and manage orders'}
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
                             </Card>
-                        </Collapse>
-
+    
+                            <Collapse in={editFormOpen} timeout='auto' unmountOnExit>
+                                <Card sx={{ boxShadow: 3 }}>
+                                    <CardContent>
+                                        <UnassignedOrders restaurantName={props.restaurantobjectprop.name}/>
+                                        <AssignedOrders restaurantName={props.restaurantobjectprop.name}/>
+                                        <CompletedOrders restaurantName={props.restaurantobjectprop.name}/>
+                                    </CardContent>
+                                </Card>
+                            </Collapse>
+    
+                        </Grid>
                     </Grid>
-                </Grid>
+    
+            </ThemeProvider>
+        )
+    } else {
+        return (
+            <ThemeProvider theme={theme}>
+                <Grid container spacing={3} style={{marginTop: 20}}>
+                        <Grid item lg={12} md={12} xs={12}>
+    
+                            <Card sx={{ boxShadow: 3}}>
+                                <CardActionArea onClick={() => setEditFormOpen(!editFormOpen)}>
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="div" textAlign='center'>
+                                            Order List
+                                            <TaskIcon style={{marginLeft: 10}}/>
+                                        </Typography>
+                                        <Typography variant="body2" color="text.secondary" textAlign='center'>
+                                            {editFormOpen? 'Click here to close the order list' : 'Click here to view and manage orders'}
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
+    
+                            <Collapse in={editFormOpen} timeout='auto' unmountOnExit>
+                                <Card sx={{ boxShadow: 3 }}>
+                                    <CardContent>
+                                        <UnassignedOrders restaurantName={props.restaurantobjectprop.name}/>
+                                        <AssignedOrders restaurantName={props.restaurantobjectprop.name}/>
+                                        <CompletedOrders restaurantName={props.restaurantobjectprop.name}/>
+                                    </CardContent>
+                                </Card>
+                            </Collapse>
+    
+                        </Grid>
+                    </Grid>
+    
+            </ThemeProvider>
+        )
+    }
 
-        </ThemeProvider>
-    )
+    
 }
 
 export default RestaurantDetails
